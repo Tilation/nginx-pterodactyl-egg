46e2a5c6012f2d75db904b68650d030689900526
Gaspar <48564654+Tilation@users.noreply.github.com>
2024-01-27 12:50:28 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/container
 echo "Hola desde el script"
-
+nginx
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
